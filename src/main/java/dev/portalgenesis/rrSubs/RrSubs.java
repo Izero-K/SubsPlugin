959433715ead53dbce4b84c9baeb307d745f6525
Izero-07 <127294953+Izero-07@users.noreply.github.com>
2025-06-17 23:22:15 +0500

    Initial commit

---
+++ src/main/java/dev/portalgenesis/rrSubs/RrSubs.java
@@ -0,0 +1,17 @@
+package dev.portalgenesis.rrSubs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RrSubs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
